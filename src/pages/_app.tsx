73a4ccddebacceec98b67db898ec3653e3db4647
Ali Ahmadi <ali.ahmadi.1377@hotmail.com>
2022-08-22 21:12:07 +0430

    theme provide with next-theme

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import 'tailwindcss/tailwind.css'
+import type { AppProps } from 'next/app'
+import Layout from '../components/Layout/Layout'
+import { ThemeProvider } from 'next-themes'
+
+function MyApp({ Component, pageProps }: AppProps) {
+
+  return (
+    <ThemeProvider enableSystem={true} attribute="class">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  )
+}
+
+export default MyApp
