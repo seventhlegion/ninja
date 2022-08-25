05f88ffe14b6827a873bb320b6b0ec131e8ef91c
Ali Ahmadi <ali.ahmadi.1377@hotmail.com>
2022-08-25 13:17:40 +0430

    transitions fixed

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 const Document = () => (
     <Html>
         <Head />
-        <body className='bg-slate-100 dark:bg-slate-900 text-slate-900 dark:text-rose-500 selection:text-blue-600 dark:selection:text-emerald-600'>
+        <body className='bg-slate-100 dark:bg-slate-900 text-slate-900 dark:text-rose-500 selection:text-blue-600 dark:selection:text-emerald-600 transition-all delay-150'>
             <Main />
             <NextScript />
         </body>
