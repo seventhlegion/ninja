cfcfa98242175d6de1d97c17bdd1699968604ca6
Ali Ahmadi <ali.ahmadi.1377@hotmail.com>
2022-08-25 17:06:39 +0430

    useLoaded added to handle first local value

---
+++ src/hooks/useLoaded.tsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from 'react'
+
+export const useLoaded = () => {
+    const [load, setLoad] = useState(false);
+
+    useEffect(() => setLoad(true), [])
+
+    return load;
+}
