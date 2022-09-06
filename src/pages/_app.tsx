a63b9fdc26d932d5a523b9ee7445fe3a96c7f7bb
Ali Ahmadi <ali.ahmadi.1377@hotmail.com>
2022-09-06 11:33:14 +0430

    Hydrate added from react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'jotai';
 import { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import Router from 'next/router';
 import NProgress from 'nprogress';
-import { ReactElement, ReactNode, StrictMode } from 'react';
+import { ReactElement, ReactNode, StrictMode, useState } from 'react';
 import 'tailwindcss/tailwind.css';
 import Layout from '../components/Layout/Layout';
 import '../styles/globals.css';
@@ -11,6 +12,8 @@ import '../styles/globals.css';
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
+
+
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -22,15 +25,20 @@ type AppPropsWithLayout = AppProps & {
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 
-  const getLayout = Component.getLayout ?? ((page) => page)
+  const [queryClient] = useState(() => new QueryClient())
 
+  const getLayout = Component.getLayout ?? ((page) => page)
 
   return getLayout(
     <StrictMode>
       <Provider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <QueryClientProvider client={queryClient}>
+          <Hydrate state={pageProps.dehydratedState}>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </Hydrate>
+        </QueryClientProvider>
       </Provider>
     </StrictMode>
   )
